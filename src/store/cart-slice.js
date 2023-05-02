f6739c16cb90368ef361edaf797675e3dbc3186a
loretari <loretari@gmail.com>
2023-05-02 11:09:47 +0300

    init: add an Action Creator Thunk

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit"
+import { uiAction} from "./ui-slice";
 
 const cartSlice = createSlice({
     name: "cart",
@@ -34,7 +35,7 @@ const cartSlice = createSlice({
             const id = action.payload
             const existingItem = state.items.find((item) => item.id === id)
             state.totalQuantity--;
-            state.changeed = true
+            // state.changeed = true
             if (existingItem.quantity === 1) {
                 state.items = state.items.filter((item) => item.id !== id)
             } else {
@@ -43,6 +44,54 @@ const cartSlice = createSlice({
         },
     },
 })
+
+
+export const sendCartData = (cart) => {
+    return async (dispatch) => {
+        dispatch(
+            uiAction.showNotification({
+            status: 'pending',
+            title: 'Sending...',
+            message: 'Sending cart data!'
+        }))
+
+        const sendRequest = async () => {
+            const response = await fetch('https://react-http-cd156-default-rtdb.firebaseio.com/cart.json', {
+                method: 'PUT',
+                body: JSON.stringify(cart),
+            });
+
+
+            if (!response.ok) {
+                throw new Error('Sending cart data failed.')
+            }
+        }
+
+
+        try {
+            await sendRequest();
+
+            dispatch(
+                uiAction.showNotification({
+                    status: 'success',
+                    title: 'Success!',
+                    message: 'Sent cart data successfully!'
+                }))
+        } catch (error) {
+            dispatch(
+                uiAction.showNotification({
+                    status: 'error',
+                    title: 'Error!',
+                    message: 'Sending cart data failed!'
+                }))
+        }
+
+
+
+
+
+    }
+}
 
 export const cartActions = cartSlice.actions
 
